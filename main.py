1c454306dede907567a51716e0d309c2b529931a
Zachary260325 <1847783965@qq.com>
2025-10-13 15:35:16 +0000

    feat: add html and fuzzy matching && add fall back && switch to a more strict formula test

--- main.py
+++ main.py
@@ -186,10 +186,10 @@ def get_test(id: str):
             "What duties do owners have for maintaining and inspecting fire service installations under regulation 8 of Cap 95B, including any exceptions?",
         ]
         return document_paths, content_type, document_metas, query
-
+    
     elif id == "formula_md" or id == "9":
-        document_path = f"oss://graxy-dev/ofnil/tmp/user/2e9946d0-eb85-4508-b499-feda899d0314/95e2ddda-f905-4d3f-abd1-8a4e9eee401f/数据结构.md"
-        filename = "数据结构.md"
+        document_path = f"oss://graxy-dev/ofnil/tmp/user/2e9946d0-eb85-4508-b499-feda899d0314/292854a2-b0a2-4bf3-95cd-4e9bccb8bf80/test-math.md"
+        filename = "test-math.md"
         content_type = "text/markdown"
         document_meta = {
             "type": "md",
@@ -199,7 +199,7 @@ def get_test(id: str):
             "createdBy": "formula_test",
             "updatedBy": "formula_test",
         }
-        query = "这道题的题解时间复杂度是什么？"
+        query = "如何理解线性运算的封闭性？"
         return document_path, content_type, document_meta, query
 
     elif id == "png" or id == "10":
